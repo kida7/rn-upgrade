8c38189133c0a940c4e294d9a028efc52661566c
Vinh Le <vinhlv@vnpay.vn>
2020-03-03 11:08:39 +0700

    publish version 1.0.14

--- src/publish.ts
+++ src/publish.ts
@@ -7,10 +7,9 @@ const argv = parser(process.argv.slice(2));
 
 (async function () {
     _package.version = argv.version || argv.v || _package.version.replace(/(\d+)$/, (match: string) => `${parseInt(match) + 1}`);
-    fs.writeFileSync('./package.json', JSON.stringify(_package, null, 2))
+    fs.writeFileSync('../package.json', JSON.stringify(_package, null, 2))
     await exec('git add -A')
     await exec(`git commit -m 'publish version ${_package.version}'`)
     await exec('tsc')
     await exec('npm publish')
-    await exec('yarn global add rn-upgrade')
 })()
